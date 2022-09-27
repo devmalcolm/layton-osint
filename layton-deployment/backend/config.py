bc521bc44d2c92e2bedb1123cc79fc0c351f862b
zaqo <96392276+zaqoQLF@users.noreply.github.com>
2022-09-28 01:59:09 +0200

    Update config.py

--- layton-deployment/backend/config.py
+++ layton-deployment/backend/config.py
@@ -1,26 +1,29 @@
 import os
 import time
+from colorama import Fore, Style, Back
 
 # version of layton (to compare to the built-in one)
 __laytonVersion__ = "0.0.2"
 
 # message for update
-__updateTrue__ = "[-] Please update the latest version of Layton"
+__updateTrueErr__ = f"[{Fore.RED}-{Style.RESET_ALL}] {Fore.WHITE}{Back.RED} INVALID VERSION OF LAYTON {Style.RESET_ALL}"
+
+__updateTrue__ = f" \r\r [{Fore.RED}!{Style.RESET_ALL}] Please update the latest version of layton !"
 
 # message for update
-__updateFalse__ ="[+] Layton has been successfully updated !"
+__updateFalse__ = f"[{Fore.GREEN}+{Style.RESET_ALL}] Layton has been successfully updated !"
 
 # error update message
 ErrUpdate = "A problem occurred while checking for an update"
 
 # github package link
-github_lnk = "https://github.com/zaqoQLF/layton"
+github_lnk = "https://github.com/zaqoQLF/layton-osint"
 
 # localhost / server link 
 localhost = 'http://127.0.0.1:5000/dashboard'
 
 # Github Raw Version of the config file (To compare version)
-raw_github = "https://raw.githubusercontent.com/zaqoQLF/layton/main/src/layton/config.py?token=GHSAT0AAAAAABYUUJWXQ7RNOGCWV4QZEDGUYZTQD5Q"
+raw_github = "https://raw.githubusercontent.com/zaqoQLF/layton-osint/master/layton-deployment/backend/config.py?token=GHSAT0AAAAAABYUUJWXNHJJP4HD5KP2O5XOYZTRLEQ"
 
 termsCond = """
 
